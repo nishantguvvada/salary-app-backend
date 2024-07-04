3eefc2a7a86458cf1d4d6d4bdc096fa3c5329579
Nishant Guvvada <nishant.guvvada@gmail.com>
2024-07-04 23:22:12 +0530

    body-Parser

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO_URL);
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
 // Signup
 app.post("/signup", signupValidation, async (req, res) => {
