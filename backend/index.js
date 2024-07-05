e3fbf40b379238b64ec54baf0d9f47cf0ddd7b54
Nishant Guvvada <nishant.guvvada@gmail.com>
2024-07-05 14:37:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO_URL);
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Signup
 app.post("/signup", signupValidation, async (req, res) => {
